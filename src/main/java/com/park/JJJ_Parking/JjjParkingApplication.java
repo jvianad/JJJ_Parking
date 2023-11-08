ec7c9d91dcf9f7982e47059f9beb33ca6e51d4be
jvianad <jeanvianademares@gmail.com>
2023-11-08 12:59:05 -0500

    Sprint 1: Configuracion inicial

---
+++ src/main/java/com/park/JJJ_Parking/JjjParkingApplication.java
@@ -0,0 +1,13 @@
+package com.park.JJJ_Parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjjParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjjParkingApplication.class, args);
+	}
+
+}
